0c8abd99b1ece3bbd2270cb0dea81f1bae56222b
“horluwaluvme” <pymfemi@gmail.com>
2022-05-06 13:34:59 -0700

    101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -27,6 +27,7 @@ int is_digit(char *s)
  *
  * Return: the length of the string
  */
+
 int _strlen(char *s)
 {
 	int i = 0;
@@ -74,17 +75,15 @@ int main(int argc, char *argv[])
 	{
 		digit1 = s1[len1] - '0';
 		carry = 0;
-		for (len2 = _strlen(s2) - 1; len2 >= 0;
-			len2--)	{
+		for (len2 = _strlen(s2) - 1; len2 >= 0; len2--)
+		{
 			digit2 = s2[len2] - '0';
-			
-			carry += result[len1 + len2 + 1]
-			+ (digit1 * digit2);
-			result[len1 + len2 + 1] = carry 			% 10;
+			carry += result[len1 + len2 + 1] + (digit1 * digit2);
+			result[len1 + len2 + 1] = carry % 10;
 			carry /= 10;
 		}
 		if (carry > 0)
-			result[len1 + len2 + 1] += carry			;
+			result[len1 + len2 + 1] += carry;
 	}
 	for (i = 0; i < len - 1; i++)
 	{
